16ea9cb5f5363976607bf757a744b06ba7e1411f
mtbstn <thushibstn24@gmail.com>
2021-08-30 18:06:23 +0530

    update Scala file

--- CaseClass.scala
+++ CaseClass.scala
@@ -1,20 +1,20 @@
 //19001207
 
-import scala.Math._
-CaseClass extends App{
-  p1 = Point(5,1)
-  p2 = Point(3,2)
-  println("The 2 points are: ")
-  println(p1)
-  println(p2)
-  println("Addition of p1 and p2: " + p1+p2 )
-  
+import scala.math._
+object CaseClass extends App{
+  val p1 = Point(5,1)
+  val p2 = Point(3,2)
+  println("\n p1 : " + p1 + "\n p2 : " + p2)
+  println("\nAddition of p1 and p2 : " + (p1+p2) )
+  println("\nMove point p1 by (2,1) : " + p1.move(2,1) )
+  println("\nDistance of p1 & p2 : " + p1.distance(p2) )
+  println("\nInvert of p2 : " + p2.invert + "\n" )
 
 }
 
 case class Point(x:Int, y:Int){
   def +(that:Point) = Point(this.x+that.x, this.y+that.y)
   def move(dx:Int, dy:Int) = Point(this.x+dx, this.y+dy)
-  def distance(that:Point): Double = (sqrt(((this.x - that.x)*(this.x - that.x)) + ((this.y - that.y)*(this.y - that.y))))
+  def distance(that:Point): Double = (sqrt((this.x - that.x)*(this.x - that.x) + (this.y - that.y)*(this.y - that.y)))
   def invert() = Point(this.y,this.x)
 }
